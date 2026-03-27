d88c243bbad8664d5ff4e8da8dea6e378da572d1
amrit <bhudathokiamrit@gmail.com>
2026-03-27 19:36:17 +0545

    fix qr url newline

--- server/src/controllers/qr.controller.js
+++ server/src/controllers/qr.controller.js
@@ -1,5 +1,5 @@
 const getQRUrl = (req, res) => {
-  const url = `${process.env.CLIENT_URL}/submit`;
+  const url = `${process.env.CLIENT_URL}/submit`.trim().replace(/\n/g, "").replace(/\r/g, "");
   res.json({ url });
 };
 
